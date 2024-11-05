39788ff05e6c14aa91057b016c3663c565319df1
ArcanoxXx-01 <dariolf03@gmail.com>
2024-11-05 04:36:08 -0500

    pequeñas mejoras de la funcion de distribucion

--- distribucion.py
+++ distribucion.py
@@ -2,13 +2,9 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-def tabla_frecuencias_y_distribucion(df, muestra, cantidad_clases):
-    # Paso 1: Calcular el rango de cada clase
-    max_valor = 100000
-    min_valor = 0
-    intervalo = (max_valor - min_valor) / cantidad_clases
+def tabla_frecuencias_y_distribucion(df, muestra, cantidad_clases,min_valor,max_valor,mostrar_grafica=False):
     
-    # Paso 2: Crear los intervalos de clase y calcular las frecuencias
+    #Crear los intervalos de clase y calcular las frecuencias
     clases = np.linspace(min_valor, max_valor, cantidad_clases + 1)
     frecuencias, _ = np.histogram(df[muestra], bins=clases)
     
@@ -18,24 +14,38 @@ def tabla_frecuencias_y_distribucion(df, muestra, cantidad_clases):
         'Frecuencia': frecuencias
     })
     
-    # Paso 3: Calcular la distribución acumulada
+    #Calcular la distribución acumulada
     tabla_frecuencias['Frecuencia Acumulada'] = tabla_frecuencias['Frecuencia'].cumsum()
     tabla_frecuencias['Distribución Acumulada'] = tabla_frecuencias['Frecuencia Acumulada'] / len(df[muestra])
     
-    # Mostrar la tabla de frecuencias
-    print("Tabla de Frecuencias:")
-    print(tabla_frecuencias)
+    # # Mostrar la tabla de frecuencias
+    # print("Tabla de Frecuencias:")
+    # print(tabla_frecuencias)
     
-    # Paso 4: Graficar la función de distribución acumulada
+    #Graficar la función de distribución
     plt.figure(figsize=(10, 6))
-    plt.plot(clases[1:], tabla_frecuencias['Frecuencia'], marker='o', color='b', linestyle='-')
-    plt.title('Función de Distribución')
+    plt.plot(clases[1:], tabla_frecuencias['Frecuencia'], marker='o', color='b', linestyle='-',label='distribucion')
+    plt.plot(clases[1:], tabla_frecuencias['Frecuencia Acumulada'], marker='o', color='red', linestyle='-',label='acumulada')    
+    plt.title('Función de Distribución & Distribución Acumulada')
     plt.xlabel('Valor')
-    plt.ylabel('Distribución Acumulada')
     plt.grid(True)
-    plt.show()
+    plt.legend()
+    if(mostrar_grafica):
+        plt.show()
+    return tabla_frecuencias
+
+#Ejemplo de uso:
 
 path = '/home/arcanox/Escritorio/proyectos/Estadistica/db/BTCUSD_d.csv'
-n=1000
-lector=pd.read_csv(path, nrows=n)
-tabla_frecuencias_y_distribucion(lector,'open',10)
+# tamaño de la muestra (últimos n dias)
+n=3500 
+# limites del rango a analisar
+min_valor = 0 ; max_valor = 100000 
+# dato del data frame que se quiere analisar
+muestra='open' 
+# crear el dataframe usando los ultimos n dias
+df=pd.read_csv(path, nrows=n)
+# indicar si graficar las funciones de distribucion
+mostrar = True
+
+tabla_frecuencias_y_distribucion(df,muestra,10,min_valor,max_valor,mostrar)
